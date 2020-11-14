077653d48c26037f378947a9a8165fc6b64b3634
Jeremy Doubleday <phate6983@gmail.com>
2020-11-14 14:08:17 -0600

    adding query params to players endpoint for positions

--- app/controllers/players/__init__.py
+++ app/controllers/players/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models.player import Player
 
 players = Blueprint("players", __name__, url_prefix="/players")
@@ -6,4 +6,10 @@ players = Blueprint("players", __name__, url_prefix="/players")
 
 @players.route("/")
 def get_players():
-    return jsonify([player.as_dict() for player in Player.query.all()])
+    pos = request.args.get('position')
+
+    if pos:
+        return jsonify([player.as_dict()for player in Player.query.filter_by(
+            position=pos.upper())])
+    else:
+        return jsonify([player.as_dict() for player in Player.query.all()])
