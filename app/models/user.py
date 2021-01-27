a2a387bbfe876e7bd2ae5db830f4fef1e0a44962
Jeremy Doubleday <phate6983@gmail.com>
2021-01-27 11:18:33 -0600

    adding

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(Password, nullable=False)
+    players_id = db.Column(db.Array, db.ForeignKey('player.id'))
 
     def __repr__(self):
         return f"<User {self.id} {self.email}>"
