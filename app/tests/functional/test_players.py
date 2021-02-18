3230b72bc78e5256f51b40094c7e38085a673b66
RockslideJr <jborden36@gmail.com>
2021-02-17 17:24:03 -0800

    fix test

--- app/tests/functional/test_players.py
+++ app/tests/functional/test_players.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import create_access_token
 
 def test_get_users(test_client, init_database):
     access_token = create_access_token('testuser')
-    response = test_client.get('/players/', headers={
+    response = test_client.get('/players', headers={
         'Authorization': 'Bearer {}'.format(access_token)
     })
     assert response.status_code == 200
