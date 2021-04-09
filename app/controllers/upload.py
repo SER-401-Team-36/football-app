53dba631fa728d35ce266f37a30f77ff0a6bd8af
csenz <casenz@hawaii.edu>
2021-04-09 08:02:13 -0700

    Only allow for csv file uploads

--- app/controllers/upload.py
+++ app/controllers/upload.py
@@ -1,11 +1,23 @@
 from flask import Blueprint, request, Response, jsonify, redirect
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models import db
+from werkzeug.utils import secure_filename
 import os
 
 upload = Blueprint("upload", __name__, url_prefix="/upload")
 
 
+def allowed_file(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit("." , 1)[1]
+
+    if ext.upper() in ["CSV"]:
+        return True
+    else:
+        return False
 
 @upload.route('/upload', methods=['POST', 'GET'])
 def upload_file():
@@ -15,11 +27,23 @@ def upload_file():
 
             file = request.files["file"]
 
-            file.save(os.path.join('../football-app', file.filename))
+            if file.filename== "":
+                print("You must name the file")
+                return redirect(request.url)
+
+            if not allowed_file(file.filename):
+                print("File type not allowed")
+                return redirect(request.url)
+
+            else:
+                filename = secure_filename(file.filename)
+                
+                file.save(os.path.join('../football-app', filename))
+                
 
             print("Saved")
 
             return redirect(request.url)
 
-    return "The item has been saved"
+    return redirect("http://localhost:3000/Upload")
     
